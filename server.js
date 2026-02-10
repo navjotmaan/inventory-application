e33b9009abc6aed7e4d1114329daf9fcf850dec1
navjotmaan <navjotmaan137@gmail.com>
2026-02-10 07:14:10 +0000

    add code to process ejs and public dir and define home route

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('node:path');
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+const public = path.join(__dirname, 'public');
+app.use(express.static(public));
+
+const homeRouter = require('./src/routes/homeRoute');
+
+app.use('/', homeRouter);
 
 const PORT = process.env.PORT || 3000
 
