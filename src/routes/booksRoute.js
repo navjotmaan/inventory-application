be3dedb8a394289fee0d5ceb5a5791637fd807af
navjotmaan <navjotmaan137@gmail.com>
2026-02-12 16:00:20 +0000

    add route for post request

--- src/routes/booksRoute.js
+++ src/routes/booksRoute.js
@@ -4,5 +4,6 @@ const booksRouter = Router();
 const booksController = require('../controllers/booksController');
 
 booksRouter.get('/:id', booksController.getAllBooks);
+booksRouter.post('/:id', booksController.addBook);
 
 module.exports = booksRouter;
--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -4,5 +4,6 @@ const homeRouter = Router();
 const homeController = require('../controllers/homeController');
 
 homeRouter.get('/', homeController.getAllGenres);
+homeRouter.post('/', homeController.addGenre);
 
 module.exports = homeRouter;
