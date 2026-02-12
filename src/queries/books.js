21c0528ee8d81e97641f6c07a9b318aba945a125
navjotmaan <navjotmaan137@gmail.com>
2026-02-12 13:44:57 +0000

    update query to get writer name

--- src/queries/books.js
+++ src/queries/books.js
@@ -1,11 +1,15 @@
 const pool = require('../config/database');
 
 async function createBook(title, price, rating, genre_id, writer_id) {
-    await pool.query("INSERT INTO books (title, price, rating) VALUES ($1, $2, $3)", [title, price, rating, genre_id, writer_id]);
+    await pool.query(`INSERT INTO books (title, price, rating, genre_id, writer_id) VALUES ($1, $2, $3, $4)`, 
+                    [title, price, rating, genre_id, writer_id]);
 }
 
 async function getAllBooks(id) {
-    const { rows } = await pool.query("SELECT * FROM books WHERE genre_id = $1", [id]);
+    const { rows } = await pool.query(`SELECT books.id, books.title, books.price, books.rating, writers.writer_name, genres.genre FROM books
+                                    JOIN writers ON books.writer_id = writers.id 
+                                    JOIN genres ON books.genre_id = genres.id
+                                    WHERE genre_id = $1`, [id]);
     return rows;
 }
 
