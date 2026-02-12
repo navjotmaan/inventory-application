e1e2ceb5c4c9e8dfcb2292d9aee152dfa33e93d3
navjotmaan <navjotmaan137@gmail.com>
2026-02-12 07:26:38 +0000

    write function to get all books of specific id

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,8 +1,14 @@
 const db = require('../queries/books');
 
 async function getAllBooks(req, res) {
-    const books = await db.getAllBooks();
-    res.render('books');
+    const id = req.params.id; 
+
+    try {
+        const books = await db.getAllBooks(id);
+        res.render('books', { title: 'Book Store', books});
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 module.exports = { getAllBooks };
