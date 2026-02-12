f223e6cf575a50d3cb552af390a522dd41d91133
navjotmaan <navjotmaan137@gmail.com>
2026-02-12 15:59:03 +0000

    write function to add book

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,16 +1,11 @@
 const db = require('../queries/books');
+const writerdb = require('../queries/writers');
 
 async function getAllBooks(req, res) {
     const id = req.params.id; 
 
     try {
         const books = await db.getAllBooks(id);
-
-        if (books.length === 0) {
-            res.send('This genre has no books.');
-            return;
-        }
-
         res.render('books', { title: 'Book Store', books});
         
     } catch (err) {
@@ -18,4 +13,19 @@ async function getAllBooks(req, res) {
     }
 }
 
-module.exports = { getAllBooks };
+async function addBook(req, res) {
+    try {
+        const writerId = await writerdb.insertWriter(req.body.writer);
+        await db.createBook(req.body.title, req.body.price, req.body.rating, req.params.id, writerId);
+        res.redirect(`/genre/${req.params.id}`);
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error saving genre");
+    }
+}
+
+module.exports = { 
+    getAllBooks,
+    addBook
+};
