0992de080d691e0c371c65299f591b7d01b774fd
navjotmaan <navjotmaan137@gmail.com>
2026-02-12 13:44:10 +0000

    add condition for no book in genre to display a message

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -5,7 +5,14 @@ async function getAllBooks(req, res) {
 
     try {
         const books = await db.getAllBooks(id);
+
+        if (books.length === 0) {
+            res.send('This genre has no books.');
+            return;
+        }
+
         res.render('books', { title: 'Book Store', books});
+        
     } catch (err) {
         console.error(err);
     }
