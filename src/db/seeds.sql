2500d7a6af681cd29ecbd69c34dfc78e12d06065
navjotmaan <navjotmaan137@gmail.com>
2026-02-12 13:48:59 +0000

    add dummy data for testing

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -1,1 +1,6 @@
-INSERT INTO genres (genre) VALUES ('Fiction'), ('Self-help');
+INSERT INTO genres (genre) VALUES ('Fiction'), ('Self-help'), ('Philosophy');
+
+INSERT INTO writers (writer_name) VALUES ('Sidney Sheldon'), ('James Clear');
+
+INSERT INTO books (title, price, rating, genre_id, writer_id) VALUES ('Nothing Lasts Forever', 250, 4, 1, 1),
+                                                                    ('Atomic Habits', 300, 4.5, 2, 2);
