c6b79da92e107f8e55b39a225366fbb67897154f
navjotmaan <navjotmaan137@gmail.com>
2026-02-12 07:19:33 +0000

    add try/catch block

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,12 @@
 const db = require('../queries/genres');
 
 async function getAllGenres(req, res) {
-    const genres = await db.getAllGeneres();
-    res.render('home', { title: 'Book Store', genres });
+    try {
+        const genres = await db.getAllGeneres();
+        res.render('home', { title: 'Book Store', genres });
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 module.exports = { getAllGenres };
