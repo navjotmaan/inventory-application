bd150d0e1f3c47be4b77de5358cd8cb2cda4082f
navjotmaan <navjotmaan137@gmail.com>
2026-02-15 11:26:36 +0000

    add post routes for updating and deleting genre

--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -5,5 +5,7 @@ const homeController = require('../controllers/homeController');
 
 homeRouter.get('/', homeController.getAllGenres);
 homeRouter.post('/', homeController.addGenre);
+homeRouter.post('/genre/update', homeController.updateGenreName);
+homeRouter.post('/genre/delete', homeController.deleteGenre);
 
 module.exports = homeRouter;
