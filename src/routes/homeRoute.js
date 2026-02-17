ac9f3b07834c5eaaacf081399566f9bcb5874288
navjotmaan <navjotmaan137@gmail.com>
2026-02-17 11:48:09 +0000

    remove /genre from update and delete routes

--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -5,7 +5,7 @@ const homeController = require('../controllers/homeController');
 
 homeRouter.get('/', homeController.getAllGenres);
 homeRouter.post('/', homeController.addGenre);
-homeRouter.post('/genre/update', homeController.updateGenreName);
-homeRouter.post('/genre/delete', homeController.deleteGenre);
+homeRouter.post('/update', homeController.updateGenreName);
+homeRouter.post('/delete', homeController.deleteGenre);
 
 module.exports = homeRouter;
