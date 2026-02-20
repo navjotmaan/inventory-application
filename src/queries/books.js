4f9652ee74d9e7659d4442fb7d8143fb7c1766f2
navjotmaan <navjotmaan137@gmail.com>
2026-02-20 14:40:55 +0000

    write query to update book by id

--- src/queries/books.js
+++ src/queries/books.js
@@ -8,8 +8,8 @@ async function createBook(title, price, rating, genre_id, writer_id) {
 async function getAllBooks(id) {
     const { rows } = await pool.query(`SELECT books.id, books.title, books.price, books.rating, books.genre_id, writers.writer_name, genres.genre FROM books
                                     JOIN writers ON books.writer_id = writers.id 
-                                    JOIN genres ON books.genre_id = genres.id
-                                    WHERE genre_id = $1`, [id]);
+                                    JOIN genres ON books.genre_id = genres.id 
+                                    WHERE genre_id = $1 ORDER BY id ASC` , [id]);
     return rows;
 }
 
@@ -21,9 +21,16 @@ async function deleteBookById(id) {
     await pool.query("DELETE FROM books WHERE id = $1", [id]);
 }
 
+async function updateBookById(id, price, rating) {
+    await pool.query(`UPDATE books
+                    SET price = $2, rating = $3
+                    WHERE id = $1`, [id, price, rating]);
+}
+
 module.exports = {
     createBook,
     getAllBooks,
     deleteAllBooks,
-    deleteBookById
+    deleteBookById,
+    updateBookById
 };
